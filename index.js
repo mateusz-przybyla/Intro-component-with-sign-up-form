e10d6d32040f49e747cb36da25bb606a9f661457
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-03-26 23:52:22 +0100

    Added else statement in form conditions

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ var password = document.getElementById("password");
 firstName.addEventListener("blur", function () {
   if (firstName.value === "") {
     document.querySelector(".nr1").innerHTML = "First Name cannot be empty";
+  } else {
+    document.querySelector(".nr1").innerHTML = "";
   }
 });
 
 lastName.addEventListener("blur", function () {
   if (lastName.value === "") {
     document.querySelector(".nr2").innerHTML = "Last Name cannot be empty";
+  } else {
+    document.querySelector(".nr2").innerHTML = "";
   }
 });
 
@@ -21,12 +25,16 @@ email.addEventListener("blur", function () {
     document.querySelector(".nr3").innerHTML = "Look like this is not an email";
     email.classList.add("ph-color");
     email.placeholder = "email@example/com";
+  } else {
+    document.querySelector(".nr3").innerHTML = "";
   }
 });
 
 password.addEventListener("blur", function () {
   if (password.value === "") {
     document.querySelector(".nr4").innerHTML = "Password cannot be empty";
+  } else {
+    document.querySelector(".nr4").innerHTML = "";
   }
 });
 
@@ -35,22 +43,35 @@ submitButton.addEventListener("click", function () {
     document.querySelector(".nr1").innerHTML = "First Name cannot be empty";
     firstName.classList.add("emptyTextBox");
     firstName.placeholder = "";
+  } else {
+    document.querySelector(".nr1").innerHTML = "";
+    firstName.classList.remove("emptyTextBox");
   }
+
   if (lastName.value === "") {
     document.querySelector(".nr2").innerHTML = "Last Name cannot be empty";
     lastName.classList.add("emptyTextBox");
     lastName.placeholder = "";
+  } else {
+    document.querySelector(".nr2").innerHTML = "";
+    lastName.classList.remove("emptyTextBox");
   }
 
   if (email.value === "") {
     document.querySelector(".nr3").innerHTML = "Look like this is not an email";
     email.classList.add("emptyTextBox", "ph-color");
     email.placeholder = "email@example/com";
+  } else {
+    document.querySelector(".nr3").innerHTML = "";
+    email.classList.remove("emptyTextBox");
   }
 
   if (password.value === "") {
     document.querySelector(".nr4").innerHTML = "Password cannot be empty";
     password.classList.add("emptyTextBox");
     password.placeholder = "";
+  } else {
+    document.querySelector(".nr4").innerHTML = "";
+    password.classList.remove("emptyTextBox");
   }
 });
